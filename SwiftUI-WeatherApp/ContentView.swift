42f211e69414ee777a194600d24deedcf21a2bc6
Aaron Cleveland <toscleveland@gmail.com>
2020-11-11 21:19:09 -0600

    Initial Commit

---
+++ SwiftUI-WeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-WeatherApp
+//
+//  Created by 09299775 on 11/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
